3c9bfa0a1225f7cc2ab8693324c7edebc324357d
Joe Burgess <bojurgess@gmail.com>
2024-10-07 17:47:37 +0100

    udp listener

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{ToSocketAddrs, UdpSocket};
+
+pub struct Server {
+    pub socket: UdpSocket,
+}
+
+impl Server {
+    pub async fn new<T: ToSocketAddrs>(addr: T) -> std::io::Result<Self> {
+        let socket = UdpSocket::bind(addr).await?;
+        Ok(Self { socket })
+    }
+
+    pub async fn listen(&self) -> std::io::Result<()> {
+        let mut buf = vec![0; 2048];
+        loop {
+            let (len, addr) = self.socket.recv_from(&mut buf).await?;
+
+            println!("Received {} bytes from {}", len, addr);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let addr: String = std::env::args()
+        .nth(1)
+        // fallback to loopback addr.
+        .unwrap_or_else(|| "127.0.0.1:20777".to_string());
+    let server = Server::new(&addr).await?;
+
+    println!("Listening on {addr}");
+    server.listen().await?;
+    Ok(())
 }
